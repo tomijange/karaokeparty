19e292b3547f058d2d777bc869fc9d5338b9874e
Thiemooo <thiemo.buedke@wgmail.de>
2020-12-26 18:39:52 +0100

    Irgendiwe waren manche Files nicht mehr da oder veraltet. Jetzt sind sie aber wieder da. 🙂

---
+++ src/client/components/CCard.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="card">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+import {Component, Vue} from "vue-property-decorator";
+@Component({
+  props: {
+    color: {
+      type: String,
+      default: 'red',
+    }
+  }
+})
+export default class CCard extends Vue {
+}
+</script>
+
+<style scoped lang="scss">
+.card {
+  box-shadow: 1px 1px 15px 5px #d7d7d7;
+  padding: 24px;
+  background-color: #fefefe;
+}
+</style>
